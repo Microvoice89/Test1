26826b4069473d302acf92f52ef036325da514b9
Microvoice <Microvoice@DESKTOP-PJ10M2P>
2018-11-21 18:29:47 +0100

    ff

---
+++ src/Test/MyExampleClassTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+public class MyExampleClassTest {
+	@Before
+	public void init () {
+		System.out.println("è iniziato il test");
+	}
+	
+	@Test
+	public void testMultiply() {
+		MyExampleClass tester = new MyExampleClass();
+	//	assertEquals(2,tester.getNum(2)); 
+		assertSame("suca",2.0,tester.getNum(2));
+		
+		}
+	  @Test(expected=java.lang.Exception.class)
+	  public void nuovoTest() throws Exception {
+	    System.out.println("Test nuovoTest");
+	    assertTrue(true);
+	    throw new Exception();
+	  }
+}
